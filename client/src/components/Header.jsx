28492036afa2a706a630162eadfab10b397a560d
4kasa786 <alkabhoyar01@gmail.com>
2025-03-31 11:59:53 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ import { FaMoon } from 'react-icons/fa'
 const Header = () => {
     return (
         <Navbar className='border-b-2 whitespace-nowrap text-sm sm:text-xl font-semibold
-         dark:text-white'>
+         dark:text-white '>
             <Link to='/' className='self-center'>
                 <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
                 via-purple-500 to-pink-500 rounded-lg text-white'>Kasa's</span>
