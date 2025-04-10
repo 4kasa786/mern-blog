aec100b2ebbc90aa56477655bedeeb4259c878eb
4kasa786 <alkabhoyar01@gmail.com>
2025-04-10 10:55:44 +0530

    added admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const signin = async (req, res, next) => {
             return next(errorHandler(400, "Invalid Credentials"));
         }
         const { password: pass, ...rest } = validUser._doc; //remove password from the response
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d' })
         res.status(200).cookie("access_token", token, {
             httpOnly: true,
         }).json(rest);
@@ -60,7 +60,7 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email });
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+            const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d' })
             const { password, ...rest } = user._doc;
             return res.status(200).cookie('access_token', token, {
                 httpOnly: true,
@@ -77,7 +77,7 @@ export const google = async (req, res, next) => {
 
             })
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d' })
             const { password, ...rest } = newUser._doc;
             return res.status(200).cookie('access_token', token, {
                 httpOnly: true,
